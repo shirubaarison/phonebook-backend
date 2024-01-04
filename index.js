f83246720bd8fc5e95ada4b70e5c3def07a7c56c
Alysson Silva <shirubaarison@gmail.com>
2024-01-03 21:13:55 -0300

    phonebook backend step7

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const morgan = require("morgan")
+
 const PORT = 3001
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
     response.send("<h1>hello</h1>")
