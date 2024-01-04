cc407567b8d19e70da53e1e5ba0d72ade289d81f
Alysson Silva <shirubaarison@gmail.com>
2024-01-04 20:41:41 -0300

    remove frontend folder

--- frontend/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ newFilter, handleNewFilter }) => {
-    return (
-      <div>
-          filter shown with <input value={newFilter} onChange={handleNewFilter}/>
-      </div>
-    )
-  }
-
-export default Filter
